130066c3292ff6db8bffb65954d650fdc65b5fe0
Kajuran <Kajuran65@gmail.com>
2025-12-29 07:34:10 -0500

    basic script for working with high vol trades

--- test.py
+++ test.py
@@ -1,14 +1,57 @@
 import requests
-import json
+import json 
+import time
 
-URL = "https://data-api.polymarket.com/trades"
 
-params = {
-    "limit": 5
-}
 
-response = requests.get(URL, params=params)
+def find_wallet():
 
-trades = response.json()
+    URL = "https://data-api.polymarket.com/trades"
 
-print(json.dumps(trades[0], indent=2))
+    params = {
+    "limit": 1000,
+    #"user":wallet
+
+    }
+    response = requests.get(URL,params=params)
+
+    trades = response.json()
+
+    whale_wallets = []
+
+
+    for trade in trades:
+
+        size = trade.get("size")
+
+        if (size > 5000):
+            whale_wallets.append(trade.get("proxyWallet"))
+
+
+    return whale_wallets
+
+
+def find_user(whale_wallets):
+
+    URL = "https://gamma-api.polymarket.com/public-profile"
+
+    userNames =[]
+
+    for wallet in whale_wallets:
+        params = {
+
+            "address": wallet,
+
+        }
+        response = requests.get(URL,params=params)
+        x = response.json()
+        
+        userNames.append(x.get("name"))
+
+    print(userNames)
+
+
+
+wal = find_wallet()
+
+find_user(wal)
