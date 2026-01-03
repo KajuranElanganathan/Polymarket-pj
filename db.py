0f7c75d2eda83cba5050ce9e4c547d24dbcb6da2
Kajuran <Kajuran65@gmail.com>
2026-01-03 03:14:05 -0500

    bug fix

--- db.py
+++ db.py
@@ -2,9 +2,13 @@ import psycopg2
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy import Column, String, Float, Integer, BigInteger, Boolean
+import os
+from dotenv import load_dotenv
 
 
-DATABASE_URL = "postgresql://admin:password@127.0.0.1:5433/polymarket"
+load_dotenv()
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
